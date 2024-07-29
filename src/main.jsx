991e26c58cf455ff716b5c93bbe2dacb33238247
ABNmmd <moha.ww.ha@gmail.com>
2024-07-29 23:10:43 +0100

    setup the files

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import {React, useState} from 'react'
+import useLocalStorage from "use-local-storage";
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home/Home.jsx'
 import Header from './components/Header/Header.jsx'
@@ -6,8 +7,9 @@ import Footer from './components/Footer/Footer.jsx'
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css'
 
-const [mode, setMode] = useState(1);
 
+const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+const [isDark, setIsDark] = useLocalStorage("isDark", preference);
 
 const router = createBrowserRouter([
   {
@@ -17,7 +19,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header mode={mode} setMode={setMode} />
+    <Header isDark={isDark} setIsDark={setIsDark} />
     <RouterProvider router={router} />
     <Footer />
   </React.StrictMode>,
