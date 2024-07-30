f5245497b28127d0d7c67c56c8cc10e155b93a47
ABNmmd <moha.ww.ha@gmail.com>
2024-07-30 20:39:35 +0100

    setup the toggle for dark/light mode

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Toggle from "react-toggle";
 
-function Header() {
+import './Header.css'
+import "react-toggle/style.css"
+
+function Header({ isDark, setIsDark }) {
   return (
-    <div>Header</div>
+    <header>
+      <div className="header-container">
+        <div className="logo">
+          <h1>AcademiaHub</h1>
+        </div>
+        <div className="container">
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <div className="icons-container">
+            <i></i>
+            <i></i>
+            <Toggle
+              checked={isDark}
+              onChange={({ target }) => setIsDark(target.checked)}
+              icons={{ checked: "🌙", unchecked: "🔆" }}
+              aria-label="Dark mode toggle"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import {React, useState} from 'react'
+import React, {useEffect} from 'react'
 import useLocalStorage from "use-local-storage";
 import ReactDOM from 'react-dom/client'
 import Home from './pages/Home/Home.jsx'
@@ -8,19 +8,32 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import './index.css'
 
 
-const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
-const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+const App = () => {
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
 
-const router = createBrowserRouter([
-  {
-    path: "/", element: <Home/>,
-  },
-]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+  ]);
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', isDark ? 'dark' : 'light');
+  }, [isDark]);
+
+  return (
+    <>
+      <Header isDark={isDark} setIsDark={setIsDark} />
+      <RouterProvider router={router} />
+      <Footer />
+    </>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Header isDark={isDark} setIsDark={setIsDark} />
-    <RouterProvider router={router} />
-    <Footer />
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
