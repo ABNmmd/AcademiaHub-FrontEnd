5f26f93225a857dd3bf38843f74ff87deed7b8ca
ABNmmd <moha.ww.ha@gmail.com>
2024-07-30 22:23:50 +0100

    importing the icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Toggle from "react-toggle";
+import { BsPersonCircle } from "react-icons/bs";
+import { IoSearchCircleOutline } from "react-icons/io5";
 
 import './Header.css'
 import "react-toggle/style.css"
@@ -9,19 +11,19 @@ function Header({ isDark, setIsDark }) {
     <header>
       <div className="header-container">
         <div className="logo">
-          <h1>AcademiaHub</h1>
+          <h1>Academia<span>Hub</span></h1>
         </div>
         <div className="container">
           <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">Blog</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Categories</a></li>
-            <li><a href="">Contact</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="categories">Categories</a></li>
+            <li><a href="contact">Contact</a></li>
           </ul>
           <div className="icons-container">
-            <i></i>
-            <i></i>
+            <IoSearchCircleOutline />
+            <BsPersonCircle />
             <Toggle
               checked={isDark}
               onChange={({ target }) => setIsDark(target.checked)}
