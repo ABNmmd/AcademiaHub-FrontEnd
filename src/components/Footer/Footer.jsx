22decc8a5bc347ae442da198409bef7ea156358a
ABNmmd <moha.ww.ha@gmail.com>
2024-08-01 16:31:21 +0100

    crating the footer structure

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
   return (
-    <div></div>
+    <footer>
+      <div className="footer-container">
+        <div className="copywrite">
+          <div className="txt">
+            <h1>Academia<span>Hub</span></h1>
+            <p>By <a href=''>Mohamed Abnoune</a> 2024. All Rights Reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
