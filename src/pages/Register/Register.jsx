09d5b66a19a4ab559f452d79e82cd447442fd6eb
ABNmmd <moha.ww.ha@gmail.com>
2024-08-02 00:30:45 +0100

    the register form

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,8 +3,28 @@ import React from 'react'
 import './Register.css'
 
 function Register() {
-  return (
-    <div>Register</div>
+  return(
+    <main>
+      <div className="form-container">
+        <div className="headline">
+          <h2>Register</h2>
+        </div>
+        <form action="" method="post">
+          <div>
+            <label htmlFor="email">E-mail</label>
+            <input type="email" name="email" id="email" />
+          </div>
+          <div>
+            <label htmlFor="username">Username</label>
+            <input type="text" name="username" id="username" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="password" name="password" id="password" />
+          </div>
+        </form>
+      </div>
+    </main>
   )
 }
 
