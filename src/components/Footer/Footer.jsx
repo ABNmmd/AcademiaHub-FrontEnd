c4d05bb3b5c946f25a4f3e7978199fa5fbd308e8
ABNmmd <moha.ww.ha@gmail.com>
2024-08-01 16:35:16 +0100

    footer links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,6 +11,11 @@ function Footer() {
             <p>By <a href=''>Mohamed Abnoune</a> 2024. All Rights Reserved.</p>
           </div>
         </div>
+        <ul className="links">
+          <li><a href="/">Terms of Use</a></li>
+          <li><a href="/">Privacy Policy</a></li>
+          <li><a href="/">Cookie Policy</a></li>
+        </ul>
       </div>
     </footer>
   )
