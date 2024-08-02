1befefd355cff0635a2e1508b2d1441170ea93c7
ABNmmd <moha.ww.ha@gmail.com>
2024-08-02 21:47:17 +0100

    setupping axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,9 @@
+require('dotenv').config();
 import axios from 'axios';
+
+// Create an Axios instance
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:3000/api',
+    withCredentials: true,
+});
+
