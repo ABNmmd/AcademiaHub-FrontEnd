e21bfbf5e30dd4ed73e3d546dc452240b5b75577
ABNmmd <moha.ww.ha@gmail.com>
2024-08-06 22:21:07 +0100

    handling the posts context

--- src/contexts/PostsContext.jsx
+++ src/contexts/PostsContext.jsx
@@ -1,3 +1,6 @@
 import { createContext, useState, useEffect } from 'react';
 import { createPost, getPosts, getPostById, updatePost, deletePost, } from '../services/api';
 
+const PostsContext = createContext();
+
+export { PostsContext,  };
