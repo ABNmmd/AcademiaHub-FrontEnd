adc540e74574329b6e742a773957cbebc1e1cdc1
ABNmmd <moha.ww.ha@gmail.com>
2024-08-06 22:50:21 +0100

    handling the posts context

--- src/contexts/PostsContext.jsx
+++ src/contexts/PostsContext.jsx
@@ -6,14 +6,29 @@ const PostsContext = createContext();
 const PostsProvider = ({ children }) => {
     const [posts, setPosts] = useState([]);
 
-    const createPost = async (postData) => {
-        try{
+    useEffect(() => {
+        const getAllPosts = async () => {
+            try {
+                const postsData = await getPosts();
+                setPosts(postsData);
+            } catch (error) {
+                console.error('Error fetching posts:', error);
+            }
+        };
+        getAllPosts();
+    }, []);
+
+    const createNewPost = async (postData) => {
+        try {
             const newPost = await createPost(postData);
             setPosts([...posts, newPost]);
-        }catch(error){
+        } catch (error) {
             console.error('Error creating post');
         }
     }
+
+
+
 }
 
 export { PostsContext, PostsProvider };
