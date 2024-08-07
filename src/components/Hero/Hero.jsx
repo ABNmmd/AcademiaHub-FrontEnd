d9bde0ef915aeca4d3a1ead64ce4e58a3b0bb720
ABNmmd <moha.ww.ha@gmail.com>
2024-08-07 23:05:40 +0100

    creating the Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,12 +8,25 @@ function Hero({ p, }) {
 
     // },[p])
 
+    const dataBox = ({data}) => {
+        return (
+            <div className="data-box">
+                <ul className="tags">
+                    {data.tags.map((tag, i) => (
+                        <li key={i}>
+                            <a href="">{tag}</a>
+                        </li>
+                    ))}
+                </ul>
+                <h1>data.title</h1>
+            </div>
+        );
+    }
+
     return (
         <section className='Hero'>
             <div className="bg-img"></div>
-            <div className="data-box" key={index}>
-
-            </div>
+            <dataBox data={p[1]} />
         </section>
     )
 }
