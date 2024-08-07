c18d5f944670f44e1514770ce0a1bf04312dc5a4
ABNmmd <moha.ww.ha@gmail.com>
2024-08-07 21:53:52 +0100

    structuring the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
 
 function Home() {
+  const p = [
+    {
+      authorId: "123",
+      title: "This Is A Blog1",
+      content: "<h1>hello welcome</h1><p>Enjoy Reading</p>",
+      tags: ["tech", "sports"],
+      likes: [],
+      dislikes: [],
+      createdAt: "22/02/2024",
+      updatedAt: "22/02/2024",
+    },
+    {
+      authorId: "123",
+      title: "This Is A Blog2",
+      content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+      tags: ["tech", "sports"],
+      likes: [],
+      dislikes: [],
+      createdAt: "22/02/2024",
+      updatedAt: "22/02/2024",
+    },
+  ];
+
   return (
-    <div></div>
+    <main>
+      <section className='Hero'>
+        <div className="bg-img"></div>
+        {p.map((item, index) => (
+          <div className="data-box">
+
+          </div>
+        ))}
+      </section>
+    </main>
   )
 }
 
