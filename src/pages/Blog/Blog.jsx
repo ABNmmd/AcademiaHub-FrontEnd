e843646ab805e28f654b15ecbeee922c3b3e2842
ABNmmd <moha.ww.ha@gmail.com>
2024-08-09 23:01:14 +0100

    changes

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,13 +1,85 @@
 import React from 'react'
 
+import PostListing from '../../components/PostListing/PostListing'
+import DataBox from '../../components/DataBox/DataBox';
+
 import './Blog.css'
 
-function Blog() {
-  return (
-    <section className='blog'>
+import bg from "../../assets/Image.png"
 
-    </section>
-  )
+function Blog() {
+    const p = [
+        {
+            authorId: "123",
+            title: "This Is A Blog1",
+            content: "<h1>hello welcome</h1><p>Enjoy Reading</p>",
+            tags: ["tech", "sports"],
+            likes: [],
+            dislikes: [],
+            createdAt: "22/02/2024",
+            updatedAt: "22/02/2024",
+        },
+        {
+            authorId: "Jason Francisco",
+            title: "The Impact of Technology on the Workplace: How Technology is Changing",
+            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+            tags: ["Technology", "Sport"],
+            likes: [],
+            dislikes: [],
+            createdAt: "22/02/2024",
+            updatedAt: "22/02/2024",
+        },
+        {
+            authorId: "Jason Francisco",
+            title: "The Impact of Technology on the Workplace: How Technology is Changing",
+            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+            tags: ["Technology", "Sport"],
+            likes: [],
+            dislikes: [],
+            createdAt: "22/02/2024",
+            updatedAt: "22/02/2024",
+        },
+        {
+            authorId: "Jason Francisco",
+            title: "The Impact of Technology on the Workplace: How Technology is Changing",
+            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+            tags: ["Technology", "Sport"],
+            likes: [],
+            dislikes: [],
+            createdAt: "22/02/2024",
+            updatedAt: "22/02/2024",
+        },
+        {
+            authorId: "Jason Francisco",
+            title: "The Impact of Technology on the Workplace: How Technology is Changing",
+            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+            tags: ["Technology", "Sport"],
+            likes: [],
+            dislikes: [],
+            createdAt: "22/02/2024",
+            updatedAt: "22/02/2024",
+        },
+    ];
+
+
+    return (
+        <main>
+            <section className='blog-hero'>
+                <div className="blog-hero-container">
+                    <div className="bg-img">
+                        <img src={p[1].img || bg} alt="" />
+                    </div>
+                    <DataBox data={p[1]} h1Class={null} />
+                </div>
+            </section>
+            <section className='blog-list'>
+                <PostListing p={p} />
+                <div className='downBtn'>
+                    <button onClick={null} className="btn">Load More</button>
+                </div>
+            </section>
+        </main>
+    )
 }
 
 export default Blog
