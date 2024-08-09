a8af56c5886ae833e3d93112d5f548c2662fb7ee
ABNmmd <moha.ww.ha@gmail.com>
2024-08-09 20:25:58 +0100

    styling the post layput section

--- src/components/PostLayout/PostLayout.jsx
+++ src/components/PostLayout/PostLayout.jsx
@@ -4,6 +4,8 @@ import DataBox from '../DataBox/DataBox'
 
 import './PostLayout.css'
 
+import bg from "../../assets/Image.png"
+
 function PostLayout({ p }) {
 
   return (
@@ -14,7 +16,7 @@ function PostLayout({ p }) {
                 p.map((post, index) => (
                     <div key={index} className="post-box">
                         <div className="image">
-                            <img src="" alt="" />
+                            <img src={bg} alt="" />
                         </div>
                         <DataBox data={post} h1Class={"h1-posts"} />
                     </div>
