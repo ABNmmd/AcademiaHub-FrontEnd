328e83ceaa8404aca0aee9712a44192a4dd13742
ABNmmd <moha.ww.ha@gmail.com>
2024-08-10 22:13:06 +0100

    changes

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -1,36 +1,20 @@
 import { useLocation, useNavigate } from 'react-router-dom';
-import useLocalStorage from "use-local-storage";
+import { useEffect } from 'react';
 
-import './Tags.css'
+import './Tags.css';
 
-function Tags({ tags }) {
-    const location = useLocation();
-    const navigate = useNavigate();
-
-    const handleTags = (tag) => {
-        const newSelectedTags = [...localStorage.getItem('selectedTags')];
-        if (newSelectedTags.includes(tag)) {
-            const index = newSelectedTags.indexOf(tag);
-            newSelectedTags.splice(index, 1);
-        } else {
-            newSelectedTags.push(tag);
-        }
-        localStorage.setItem('selectedTags', newSelectedTags);
-
-        if (location.pathname !== '/categories') {
-            navigate('/categories');
-        }
-    }
-
-    return (
-        <ul className="tags">
-            {tags.map((tag, i) => (
-                <li key={i}>
-                    <button className={selectedTags.includes(tag) ? 'active' : ''} onClick={() => handleTags(tag)}>{tag}</button>
-                </li>
-            ))}
-        </ul>
-    );
+function Tags({ tags, onTagClick }) {
+  return (
+    <ul className="tags">
+      {tags.map((tag, i) => (
+        <li key={i}>
+          <button className={selectedTags.includes(tag) ? 'a active' : 'a'} onClick={() => onTagClick(tag)}>
+            {tag}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default Tags
+export default Tags;
