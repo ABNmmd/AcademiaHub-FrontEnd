ca04a9f929f3a3cf15450ad02e639ef914efc366
ABNmmd <moha.ww.ha@gmail.com>
2024-08-12 22:22:41 +0100

    creat the write page

--- src/pages/Write/Write.jsx
+++ src/pages/Write/Write.jsx
@@ -6,11 +6,16 @@ import './Write.css'
 
 function Write() {
     const [value, setValue] = useState('');
-  return (
-    <main>
-
-    </main>
-  )
+    return (
+        <main>
+            <section className='write-content'>
+                <ReactQuill theme="snow" value={value} onChange={setValue} />;
+            </section>
+            <section>
+                
+            </section>
+        </main>
+    )
 }
 
 export default Write
