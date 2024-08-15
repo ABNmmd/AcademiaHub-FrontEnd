e3219aee052067726827aa615d6a5bf78fa19719
ABNmmd <moha.ww.ha@gmail.com>
2024-08-15 21:20:27 +0100

    styling the interactions comp

--- src/components/PostInteraction/PostInteraction.jsx
+++ src/components/PostInteraction/PostInteraction.jsx
@@ -5,8 +5,8 @@ import { SlLike, SlDislike, SlBubble } from "react-icons/sl";
 
 import './PostInteraction.css'
 
-function PostInteraction({ showComment, setShowComment }) {
-    const {  } = useContext(PostsContext);
+function PostInteraction({ likes, dislikes, showComment, setShowComment }) {
+    const { } = useContext(PostsContext);
     const handleLike = () => {
 
     };
@@ -20,11 +20,19 @@ function PostInteraction({ showComment, setShowComment }) {
     return (
         <div className='interactions'>
             <div className="di-like">
-                <button onClick={handleLike}><SlLike /></button>
-                <button onClick={handleDiLike}><SlDislike /></button>
+                <button onClick={handleLike}>
+                    <span>{likes}</span>
+                    <SlLike />
+                </button>
+                <button onClick={handleDiLike}>
+                    <span>{dislikes}</span>
+                    <SlDislike />
+                </button>
             </div>
             <div className="com">
-                <button onClick={handleCommentClick}><SlBubble /></button>
+                <button onClick={handleCommentClick}>
+                    <SlBubble />
+                </button>
             </div>
         </div>
     )
