1774aedb5ff4327282d551a2b32e8f16711ed164
ABNmmd <moha.ww.ha@gmail.com>
2024-08-15 21:42:23 +0100

    creat the recamanded posts comp

--- src/components/RecPosts/RecPosts.jsx
+++ src/components/RecPosts/RecPosts.jsx
@@ -4,8 +4,20 @@ import Slider from "react-slick";
 import './RecPosts.css'
 
 function RecPosts() {
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    };
+
     return (
-        <div>RecPosts</div>
+        <div className="slider-container">
+            <Slider {...settings}>
+                
+            </Slider>
+        </div>
     )
 }
 
