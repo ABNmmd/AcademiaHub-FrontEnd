9d184c79a205954c7de81448d477c4a25da6ed71
ABNmmd <moha.ww.ha@gmail.com>
2024-08-15 22:25:17 +0100

    creat the recamanded posts comp

--- src/components/RecPosts/RecPosts.jsx
+++ src/components/RecPosts/RecPosts.jsx
@@ -1,9 +1,67 @@
 import React from 'react'
 import Slider from "react-slick";
 
+import DataBox from '../DataBox/DataBox';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './RecPosts.css'
+import bg from "../../assets/Image.png"
 
 function RecPosts({ tags }) {
+    const p = [
+        {
+            authorId: "123",
+            title: "This Is A Blog1",
+            content: "<h1>hello welcome</h1><p>Enjoy Reading</p>",
+            tags: ["tech", "sports"],
+            likes: [],
+            dislikes: [],
+            createdAt: "22/02/2024",
+            updatedAt: "22/02/2024",
+        },
+        {
+            authorId: "Jason Francisco",
+            title: "The Impact of Technology on the Workplace: How Technology is Changing",
+            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+            tags: ["Technology", "Sport"],
+            likes: [],
+            dislikes: [],
+            createdAt: "22/02/2024",
+            updatedAt: "22/02/2024",
+        },
+        {
+            authorId: "Jason Francisco",
+            title: "The Impact of Technology on the Workplace: How Technology is Changing",
+            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+            tags: ["Technology", "Sport"],
+            likes: [],
+            dislikes: [],
+            createdAt: "22/02/2024",
+            updatedAt: "22/02/2024",
+        },
+        {
+            authorId: "Jason Francisco",
+            title: "The Impact of Technology on the Workplace: How Technology is Changing",
+            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+            tags: ["Technology", "Sport"],
+            likes: [],
+            dislikes: [],
+            createdAt: "22/02/2024",
+            updatedAt: "22/02/2024",
+        },
+        {
+            authorId: "Jason Francisco",
+            title: "The Impact of Technology on the Workplace: How Technology is Changing",
+            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+            tags: ["Technology", "Sport"],
+            likes: [],
+            dislikes: [],
+            createdAt: "22/02/2024",
+            updatedAt: "22/02/2024",
+        },
+    ];
+
     const settings = {
         dots: true,
         infinite: true,
@@ -16,7 +74,14 @@ function RecPosts({ tags }) {
         <div className="slider-container">
             <Slider {...settings}>
                 {
-
+                    p.map((post, index) => (
+                        <div key={index} className="post-box">
+                            <div className="image">
+                                <img src={bg} alt="" />
+                            </div>
+                            <DataBox data={post} h1Class={"h1-posts"} />
+                        </div>
+                    ))
                 }
             </Slider>
         </div>
