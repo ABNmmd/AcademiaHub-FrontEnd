21b0ae1c8f16fd3df554f69ffc23316ab4a33834
ABNmmd <moha.ww.ha@gmail.com>
2024-08-16 16:28:47 +0100

    handling comments withe axios

--- src/services/api.js
+++ src/services/api.js
@@ -115,6 +115,11 @@ const deletePost = async (id) => {
 
 //-------------
 
+// API call to creat a comment
+const createComment = async (commentData) => {
+    
+}
+
 export {
     register,
     login,
