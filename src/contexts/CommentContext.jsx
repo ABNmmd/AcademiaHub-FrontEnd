f7c353976c25f758e7b8da9b1fccdfb510efa36b
ABNmmd <moha.ww.ha@gmail.com>
2024-08-16 17:15:38 +0100

    handling comment context

--- src/contexts/CommentContext.jsx
+++ src/contexts/CommentContext.jsx
@@ -1,2 +1,6 @@
+import { createContext } from 'react';
 import { createComment, getComments, updateComment, deleteComment } from '../services/api';
 
+const PostsContext = createContext();
+
+const PostsProvider = ({ children }) => {}
