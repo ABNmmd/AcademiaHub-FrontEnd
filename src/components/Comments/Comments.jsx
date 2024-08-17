36824c271ba487482649b89356415b36be6d80dd
ABNmmd <moha.ww.ha@gmail.com>
2024-08-17 21:09:40 +0100

    creat comment section

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -9,33 +9,38 @@ function Comments({ postId, showComment }) {
     return (
         <section className="comments">
             <div className="comments-container">
-                <div className="comment-edit-box">
-                    <div className="user">
-                        <img src={prf} alt="" />
-                    </div>
-                    <div className="cont">
-                        <span>Jhon Doe</span>
-                        <input type="text" placeholder='Add a comment' />
-                    </div>
-                    <div className="send-btn">
-                        <button type="submit"></button>
-                    </div>
+                <div className="head">
+                    <h3>1033 comments</h3>
                 </div>
-                {
-                    showComment &&
-                    comments.map((com, index) => (
-                        <div className="comment-box" key={index}>
-                            <div className="user">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="cont">
-                                <span>Jhon Doe</span><span className='time'>2 days ago</span>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                <div className="inter"></div>
-                            </div>
+                <div className='body'>
+                    <div className="comment-edit-box">
+                        <div className="user">
+                            <img src={prf} alt="" />
                         </div>
-                    ))
-                }
+                        <div className="cont">
+                            <span>Jhon Doe</span>
+                            <input type="text" placeholder='Add a comment' />
+                        </div>
+                        <div className="send-btn">
+                            <button type="submit"></button>
+                        </div>
+                    </div>
+                    {
+                        showComment &&
+                        comments.map((com, index) => (
+                            <div className="comment-box" key={index}>
+                                <div className="user">
+                                    <img src="" alt="" />
+                                </div>
+                                <div className="cont">
+                                    <span>Jhon Doe</span><span className='time'>2 days ago</span>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                                    <div className="inter"></div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </section>
     )
