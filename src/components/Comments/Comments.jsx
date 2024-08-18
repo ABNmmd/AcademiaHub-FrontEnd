2b7eee34b313bda4dba28d1f69812100cfb7fce1
ABNmmd <moha.ww.ha@gmail.com>
2024-08-18 19:34:29 +0100

    creat comment section

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -59,20 +59,21 @@ function Comments({ postId, showComment }) {
                                 <button type="submit">Send</button>
                             </div>}
                         </div>
-                        <div className="send-btn">
-                            <button type="submit"></button>
-                        </div>
                     </div>
                     {
                         comments.map((com, index) => (
                             <div className="comment-box" key={index}>
                                 <div className="user">
-                                    <img src="" alt="" />
+                                    <img src={prf} alt="" />
                                 </div>
                                 <div className="cont">
                                     <span>Jhon Doe</span><span className='time'>2 days ago</span>
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                                    <div className="inter"></div>
+                                    <div className="inter">
+                                        <button></button>
+                                        <button></button>
+                                        <button></button>
+                                    </div>
                                 </div>
                             </div>
                         ))
