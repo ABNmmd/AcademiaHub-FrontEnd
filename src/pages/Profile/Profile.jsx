5561482301b8bc72878b68981cc2ba28c5400375
ABNmmd <moha.ww.ha@gmail.com>
2024-08-21 23:34:19 +0100

    handling user profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
 
+import UserInfo from '../../components/UserInfo/UserInfo';
+
 import './Profile.css'
 
 function Profile() {
     const { userId } = useParams();
+    
     return (
         <main>
-
+            <UserInfo />
         </main>
     )
 }
