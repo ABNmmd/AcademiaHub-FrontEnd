a841f0244760953d813c6f09f9a0363473b05538
ABNmmd <moha.ww.ha@gmail.com>
2024-08-21 23:57:42 +0100

    handling user profile

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 import './UserInfo.css'
 
+import bg from "../../assets/download.png"
+
 function UserInfo({ author }) {
     return (
         <section className='userInfo'>
             <div className="info-container">
                 <div>
-                    <img src="" alt="" />
-                    
+                    <img src={bg} alt="" />
+
                 </div>
             </div>
         </section>
