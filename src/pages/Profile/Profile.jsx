be38dbb999921a63482d5ad31c60d707f806fac5
ABNmmd <moha.ww.ha@gmail.com>
2024-08-21 23:37:47 +0100

    handling user profile

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import { UserContext } from '../../contexts/UserContext';
 
 import UserInfo from '../../components/UserInfo/UserInfo';
 
@@ -7,6 +8,20 @@ import './Profile.css'
 
 function Profile() {
     const { userId } = useParams();
+    const {  } = useContext(UserContext);
+    
+    useEffect(() => {
+        const getAuther = async () => {
+            try {
+                const postData = await get(postId);
+                console.log('post :', postData);
+                setPost(postData);
+            } catch (error) {
+                console.log(`Error fitshing post with id: ${postId}`, error);
+            }
+        };
+        getAuther();
+    }, [userId])
     
     return (
         <main>
