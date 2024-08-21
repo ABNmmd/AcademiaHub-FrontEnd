57b3dca02577afe523474a3eb74bd0977738013c
ABNmmd <moha.ww.ha@gmail.com>
2024-08-21 19:45:12 +0100

    handling recamanded posts

--- src/components/RecPosts/RecPosts.jsx
+++ src/components/RecPosts/RecPosts.jsx
@@ -9,59 +9,61 @@ import './RecPosts.css'
 import bg from "../../assets/Image.png"
 
 function RecPosts({ tags }) {
+    const [p, setP] = useState([]);
     const [recPosts, setRecPosts] = useState([]);
-    const p = [
-        {
-            authorId: "123",
-            title: "This Is A Blog1",
-            content: "<h1>hello welcome</h1><p>Enjoy Reading</p>",
-            tags: ["tech", "sports"],
-            likes: [],
-            dislikes: [],
-            createdAt: "22/02/2024",
-            updatedAt: "22/02/2024",
-        },
-        {
-            authorId: "Jason Francisco",
-            title: "The Impact of Technology on the Workplace: How Technology is Changing",
-            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
-            tags: ["Technology", "Sport"],
-            likes: [],
-            dislikes: [],
-            createdAt: "22/02/2024",
-            updatedAt: "22/02/2024",
-        },
-        {
-            authorId: "Jason Francisco",
-            title: "The Impact of Technology on the Workplace: How Technology is Changing",
-            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
-            tags: ["Technology", "Sport"],
-            likes: [],
-            dislikes: [],
-            createdAt: "22/02/2024",
-            updatedAt: "22/02/2024",
-        },
-        {
-            authorId: "Jason Francisco",
-            title: "The Impact of Technology on the Workplace: How Technology is Changing",
-            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
-            tags: ["Technology", "Sport"],
-            likes: [],
-            dislikes: [],
-            createdAt: "22/02/2024",
-            updatedAt: "22/02/2024",
-        },
-        {
-            authorId: "Jason Francisco",
-            title: "The Impact of Technology on the Workplace: How Technology is Changing",
-            content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
-            tags: ["Technology", "Sport"],
-            likes: [],
-            dislikes: [],
-            createdAt: "22/02/2024",
-            updatedAt: "22/02/2024",
-        },
-    ];
+    const { getOnePost } = useContext(PostsContext);
+    // const p = [
+    //     {
+    //         authorId: "123",
+    //         title: "This Is A Blog1",
+    //         content: "<h1>hello welcome</h1><p>Enjoy Reading</p>",
+    //         tags: ["tech", "sports"],
+    //         likes: [],
+    //         dislikes: [],
+    //         createdAt: "22/02/2024",
+    //         updatedAt: "22/02/2024",
+    //     },
+    //     {
+    //         authorId: "Jason Francisco",
+    //         title: "The Impact of Technology on the Workplace: How Technology is Changing",
+    //         content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+    //         tags: ["Technology", "Sport"],
+    //         likes: [],
+    //         dislikes: [],
+    //         createdAt: "22/02/2024",
+    //         updatedAt: "22/02/2024",
+    //     },
+    //     {
+    //         authorId: "Jason Francisco",
+    //         title: "The Impact of Technology on the Workplace: How Technology is Changing",
+    //         content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+    //         tags: ["Technology", "Sport"],
+    //         likes: [],
+    //         dislikes: [],
+    //         createdAt: "22/02/2024",
+    //         updatedAt: "22/02/2024",
+    //     },
+    //     {
+    //         authorId: "Jason Francisco",
+    //         title: "The Impact of Technology on the Workplace: How Technology is Changing",
+    //         content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+    //         tags: ["Technology", "Sport"],
+    //         likes: [],
+    //         dislikes: [],
+    //         createdAt: "22/02/2024",
+    //         updatedAt: "22/02/2024",
+    //     },
+    //     {
+    //         authorId: "Jason Francisco",
+    //         title: "The Impact of Technology on the Workplace: How Technology is Changing",
+    //         content: "<h1>hello welcome again</h1><p>Enjoy Reading</p>",
+    //         tags: ["Technology", "Sport"],
+    //         likes: [],
+    //         dislikes: [],
+    //         createdAt: "22/02/2024",
+    //         updatedAt: "22/02/2024",
+    //     },
+    // ];
 
     useEffect(() => {
         setRecPosts(p.filter((post) =>
