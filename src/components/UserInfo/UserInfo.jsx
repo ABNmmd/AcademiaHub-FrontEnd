69ff6bb79a5f1c09177eac9069a8c7d29800d40b
ABNmmd <moha.ww.ha@gmail.com>
2024-08-22 21:31:45 +0100

    handling the author profile page

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -10,19 +10,31 @@ function UserInfo({ author }) {
     const { user } = useContext(UserContext);
     return (
         <section className='userInfo'>
-            { }
-            <div className="info-container">
-                {author?._id == user?._id &&
-                    <button className="i">
-                        <MdModeEdit />
-                    </button>
-                }
-                <div className='author'>
-                    <img src={bg} alt="" />
-                    <h2>{author?.username}</h2>
+            {editMode
+                ? <div className="info-container">
+                    <div className='author'>
+                        <div>
+                            <img src={bg} alt="" />
+                            <button></button>
+                        </div>
+                        <input type="text" value={author?.username} />
+                    </div>
+                    <p>{author?.bio || 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, id. Doloremque exercitationem ipsa explicabo ex hic vero excepturi rerum eveniet, ipsum, consequuntur maxime ullam odio quod architecto enim eius modi!'}</p>
                 </div>
-                <p>{author?.bio || 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, id. Doloremque exercitationem ipsa explicabo ex hic vero excepturi rerum eveniet, ipsum, consequuntur maxime ullam odio quod architecto enim eius modi!'}</p>
-            </div>
+                : <div className="info-container">
+                    {author?._id == user?._id &&
+                        <button className="i">
+                            <MdModeEdit />
+                        </button>
+                    }
+                    <div className='author'>
+                        <img src={bg} alt="" />
+                        <h2>{author?.username}</h2>
+                    </div>
+                    <p>{author?.bio || 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, id. Doloremque exercitationem ipsa explicabo ex hic vero excepturi rerum eveniet, ipsum, consequuntur maxime ullam odio quod architecto enim eius modi!'}</p>
+                </div>
+            }
+
         </section>
     )
 }
