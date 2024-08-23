f54fde911e4c2613e73d27d60b69f693ec687f20
ABNmmd <moha.ww.ha@gmail.com>
2024-08-23 23:57:35 +0100

    handle post edit page

--- src/main.jsx
+++ src/main.jsx
@@ -65,7 +65,7 @@ const App = () => {
       element: <Posts />,
       children: [
         {
-          path: "edit",
+          path: "edit/:postId",
           element: <PostsEdit />,
         },
       ],
