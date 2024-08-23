fc6918e9286f4099d1da1ce8515185de572edffa
ABNmmd <moha.ww.ha@gmail.com>
2024-08-23 23:18:22 +0100

    handle post edit page

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import Login from './pages/Login/Login.jsx'
 import Categories from './pages/Categories/Categories.jsx';
 import Write from './pages/Write/Write.jsx';
 import Posts from './pages/Posts/Posts.jsx';
+import PostsEdit from './pages/PostsEdit/PostsEdit.jsx';
 import Profile from './pages/Profile/Profile.jsx';
 
 import Header from './components/Header/Header.jsx'
