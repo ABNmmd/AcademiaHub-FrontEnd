b4ad90ef0d5599cca4dbb7e86f95c441eab2ab72
ABNmmd <moha.ww.ha@gmail.com>
2024-08-24 10:47:03 +0100

    handling search page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PostListing from '../../components/PostListing/PostListing'
+import { PostsContext } from '../../contexts/PostsContext';
 
 import './Search.css'
 
 function Search() {
+    const {  } = useContext(PostsContext);
     return (
         <main>
             <PostListing p={} />
