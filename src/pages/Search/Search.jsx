893ffb354b4cc826da4b4865b7582eb5c6b08d1d
ABNmmd <moha.ww.ha@gmail.com>
2024-08-24 10:45:55 +0100

    handling search page

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import PostListing from '../../components/PostListing/PostListing'
 
 import './Search.css'
 
 function Search() {
     return (
         <main>
-
+            <PostListing />
         </main>
     )
 }
