d9726c361a7d584cf72e05d7f7212a6979caf651
ABNmmd <moha.ww.ha@gmail.com>
2024-08-24 06:39:50 +0100

    handle the post edit page

--- src/pages/PostsEdit/PostsEdit.jsx
+++ src/pages/PostsEdit/PostsEdit.jsx
@@ -5,7 +5,44 @@ import './PostsEdit.css'
 function PostsEdit() {
     return (
         <main>
+            <section className='write-content'>
+                <div className="title">
+                    <h2><label htmlFor="title">Title</label></h2>
+                    <input
+                        type="text"
+                        className='title'
+                        id='title'
+                        onChange={handleTitle}
+                    />
+                </div>
+                <div className="content">
+                    <h2>Content</h2>
+                    <ReactQuill theme="snow" value={content} onChange={setContent} />
+                </div>
+                <div className="tags">
+                    <h2>Tags</h2>
+                    <Select
+                        options={options}
+                        defaultValue={[]}
+                        isMulti
+                        name="tags"
+                        className="basic-multi-select"
+                        classNamePrefix="select"
+                        onChange={handleTags}
+                        styles={{
+                            control: (baseStyles, state) => ({
+                                ...baseStyles,
+                                borderColor: 'var(--border)',
+                                backgroundColor: 'var(--background)',
+                            }),
+                        }}
+                    />
+                </div>
 
+                <div className="buttons">
+                    <button type="submit" onClick={handleCreatePost}>Publish</button>
+                </div>
+            </section>
         </main>
     )
 }
