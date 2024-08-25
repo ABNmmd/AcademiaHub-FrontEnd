f430199c9fc5469f7d38af103d0a3d360ed39cf5
ABNmmd <moha.ww.ha@gmail.com>
2024-08-25 20:32:39 +0100

    Search page handling

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -6,7 +6,8 @@ import './Search.css'
 
 function Search() {
     const { posts } = useContext(PostsContext);
-    const [searchedP, setSearchP] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [searchedP, setSearchedP] = useState([]);
     const [searchKeyWord, setSearchKeyWord] = useState([]);
 
     const handleKeyWords = (e) => {
@@ -14,8 +15,14 @@ function Search() {
         setSearchKeyWord(sString.split(' '));
     }
 
-    const handleSearchBtn = () => {
-        
+    const handleSearchBtn = (e) => {
+        e.preventDefault();
+        const foundPosts = posts.filter((post) =>
+            searchKeyWord.every((word) => post.title.includes(word))
+        );
+        console.log(foundPosts);
+        setSearchedP(foundPosts);
+        setShowResult(true);
     }
 
     return (
@@ -23,13 +30,13 @@ function Search() {
             <section className="search-form">
                 <form action="">
                     <input type="search" onChange={handleKeyWords} name="search" id="search" placeholder='Search here...' />
-                    <button type="button" onClick={handleSearchBtn}></button>
+                    <button type="submit" onClick={handleSearchBtn}></button>
                 </form>
             </section>
-            <section className="search-result">
+            {showResult && <section className="search-result">
                 <h2>Search Result Of {searchKeyWord.join(' ')}</h2>
                 <PostListing p={searchedP} />
-            </section>
+            </section>}
         </main>
     )
 }
