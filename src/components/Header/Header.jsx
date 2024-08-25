7472875279e77691432b2b4e249b6f48c7b04bfc
ABNmmd <moha.ww.ha@gmail.com>
2024-08-25 23:41:47 +0100

    handle Newsletter section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -29,7 +29,7 @@ function Header({ isDark, setIsDark }) {
             <li><a href="/">Home</a></li>
             <li><a href="/categories">Categories</a></li>
             <li><a href="#about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><a href="#newsletter">Newsletter</a></li>
           </ul>
           <div className="icons-container">
             <div>
