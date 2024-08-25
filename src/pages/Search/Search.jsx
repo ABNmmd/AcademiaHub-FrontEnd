5229cab3993ce7c498c21151f8ee0c2ba6928a47
ABNmmd <moha.ww.ha@gmail.com>
2024-08-25 18:57:03 +0100

    Search page handling

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -16,7 +16,7 @@ function Search() {
                 </form>
             </section>
             <section className="search-result">
-                <h2>Search Result of: {}</h2>
+                <h2>Search Result Of {}</h2>
                 <PostListing p={searchedP} />
             </section>
         </main>
