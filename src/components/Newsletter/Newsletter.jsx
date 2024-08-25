70ebbd94cd2f86bc4d91d1fce59f7b8c53f12114
ABNmmd <moha.ww.ha@gmail.com>
2024-08-25 23:54:15 +0100

    handle Newsletter section

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -5,7 +5,16 @@ import './Newsletter.css'
 function Newsletter() {
     return (
         <section id='newsletter'>
-
+            <div className="form-container">
+                <div className='headline'>
+                    <h2>Weekly Newsletter</h2>
+                    <p>Get blog articles and offers via email</p>
+                </div>
+                <form>
+                    <input type="text" />
+                    <button type="submit">Subscribe</button>
+                </form>
+            </div>
         </section>
     )
 }
