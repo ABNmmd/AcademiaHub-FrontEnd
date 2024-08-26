eec56ab3411fabf6475249c4779fe2e8deb5ce6c
ABNmmd <moha.ww.ha@gmail.com>
2024-08-26 23:31:08 +0100

    handle comments update

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -248,16 +248,21 @@ function Comments({ postId }) {
                                             <div className="cont">
                                                 <span>{com.authorId?.username}</span><span className='time'>{moment(com.createdAt).fromNow()}</span>
                                                 <p>{com.content}</p>
-                                                {com.authorId?._id === (user ? user._id : null) && (
-                                                    <div className='onerOptions'>
-                                                        <i><MdMoreVert /></i>
-                                                        <ul role="menu">
-                                                            <li role="menuitem"><button onClick={() => handleEditClick(com._id)}><CiEdit /> Edit</button></li>
-                                                            <li role="menuitem"><button onClick={() => handleCommentDel(com._id)}><MdDelete /> Delete</button></li>
-                                                        </ul>
-                                                    </div>
-                                                )}
-                                                {intError && <p style={{ color: 'red', fontSize: '12px' }}>{intError}</p>}
+                                                {delError && <p>{delError}</p>}
+                                                <div className="inter">
+                                                    <button className={com.likes?.includes(user ? user._id : null) ? 'interacted' : null}><BiSolidLike /> {com.likes?.length}</button>
+                                                    <button className={com.dislikes?.includes(user ? user._id : null) ? 'interacted' : null}><BiSolidDislike /> {com.dislikes?.length}</button>
+                                                    {com.authorId?._id == (user ? user._id : null) &&
+                                                        <div className='onerOptions'>
+                                                            <i><MdMoreVert /></i>
+                                                            <ul role="menu">
+                                                                <li role="menuitem"><button><CiEdit /> Edit</button></li>
+                                                                <li role="menuitem"><button onClick={() => handleCommentDel(com._id)}><MdDelete /> Delete</button></li>
+                                                            </ul>
+                                                        </div>
+                                                    }
+                                                    {intError && <p style={{ color: 'red', fontSize: '12px' }}>{intError}</p>}
+                                                </div>
                                             </div>
                                         )}
                                 </div>
