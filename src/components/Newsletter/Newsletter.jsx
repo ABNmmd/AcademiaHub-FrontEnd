005b545cd85d324ca07e7f2a5f3eb5cc5d5893d5
ABNmmd <moha.ww.ha@gmail.com>
2024-08-26 21:27:42 +0100

    handle home page

--- src/components/Newsletter/Newsletter.jsx
+++ src/components/Newsletter/Newsletter.jsx
@@ -33,7 +33,7 @@ function Newsletter() {
                         <input type="email" name="entry.507768606" id="email" placeholder='E-mail' onChange={(e) => setEmail(e.target.value)} />
                         <CiMail />
                     </div>
-                    {error && <p>{error}</p>}
+                    {!error && <p>{error}</p>}
                     <button type="submit">Subscribe</button>
                 </form>
                 <iframe name="hidden_iframe" id="hidden_iframe" style={{ display: 'none' }}></iframe>
