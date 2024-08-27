e8e87b32593b3335c8cf77d09f93a57a50daff5b
ABNmmd <moha.ww.ha@gmail.com>
2024-08-27 22:19:05 +0100

    hanling the interactions

--- src/components/PostInteraction/PostInteraction.jsx
+++ src/components/PostInteraction/PostInteraction.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { UserContext } from '../../contexts/UserContext';
 import { PostsContext } from '../../contexts/PostsContext';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -13,6 +13,7 @@ function PostInteraction({ autherId, likes, dislikes }) {
     const { deleteExistingPost, likePostAction, dislikePostAction } = useContext(PostsContext);
     const { isAuth, user } = useContext(UserContext);
     const navigate = useNavigate();
+    const [error, setError] = useState('');
 
     const handleLike = async () => {
         try {
@@ -43,34 +44,37 @@ function PostInteraction({ autherId, likes, dislikes }) {
     };
 
     return (
-        <div className='interactions'>
-            <div className="di-like">
-                <button className={dislikes?.includes(user._id) && 'activeAction'} onClick={handleLike}>
-                    <span>{likes?.length}</span>
-                    <SlLike />
-                </button>
-                <button className={dislikes?.includes(user._id) && 'activeAction'} onClick={handleDislike}>
-                    <span>{dislikes?.length}</span>
-                    <SlDislike />
-                </button>
+        <>
+            <div className='interactions'>
+                <div className="di-like">
+                    <button className={dislikes?.includes(user._id) && 'activeAction'} onClick={handleLike}>
+                        <span>{likes?.length}</span>
+                        <SlLike />
+                    </button>
+                    <button className={dislikes?.includes(user._id) && 'activeAction'} onClick={handleDislike}>
+                        <span>{dislikes?.length}</span>
+                        <SlDislike />
+                    </button>
+                </div>
+                <div className="com">
+                    <button>
+                        <a href="#comments">
+                            <SlBubble />
+                        </a>
+                    </button>
+                    {isAuth && (user?._id == autherId?._id) &&
+                        <div className="x">
+                            <SlOptionsVertical />
+                            <ul>
+                                <li><a href={`edit/${postId}`}><CiEdit />Edit</a></li>
+                                <li><button onClick={handlePostDelete}><MdDelete />Delete</button></li>
+                            </ul>
+                        </div>
+                    }
+                </div>
             </div>
-            <div className="com">
-                <button>
-                    <a href="#comments">
-                        <SlBubble />
-                    </a>
-                </button>
-                {isAuth && (user?._id == autherId?._id) &&
-                    <div className="x">
-                        <SlOptionsVertical />
-                        <ul>
-                            <li><a href={`edit/${postId}`}><CiEdit />Edit</a></li>
-                            <li><button onClick={handlePostDelete}><MdDelete />Delete</button></li>
-                        </ul>
-                    </div>
-                }
-            </div>
-        </div>
+            {error && <p className='error'>{error}this is error</p>}
+        </>
     )
 }
 
