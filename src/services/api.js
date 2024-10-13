3227cb1f1ea4fe78b5ccfe4e4ace1bd1c63064eb
ABNmmd <moha.ww.ha@gmail.com>
2024-10-13 20:59:06 +0100

    test

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance
 const api = axios.create({
-    baseURL: import.meta.env.REACT_APP_API_URL || 'http://localhost:3000/api',
+    baseURL: import.meta.env.REACT_APP_API_URL,
     withCredentials: true,
 });
 
